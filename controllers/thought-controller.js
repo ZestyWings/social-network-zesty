d8a2146ec4b080c530b1505322b7b57ad625828e
Zeth <zethsatentes@yahoo.com>
2022-05-24 22:51:13 -0700

    Controller JS

---
+++ controllers/thought-controller.js
@@ -0,0 +1,130 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+  // get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // get single thought by ID
+  getThoughtsById(req, res) {
+    Thought.findOne({
+      _id: req.params.id,
+    })
+      .select("-__v")
+      .sort({
+        _id: -1,
+      })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({
+            message: "Not found with id",
+          });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // post create new thought
+  createThought(req, res) {
+    Thought.create(body)
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          {
+            _id: req.body.userId,
+          },
+          {
+            $addToSet: {
+              thoughts: dbThoughtData._id,
+            },
+          },
+          {
+            new: true,
+          }
+        );
+      })
+      .then((dbUsersData) => {
+        if (!dbUsersData) {
+          res.status(404).json({
+            message: "No user found",
+          });
+          return;
+        }
+        res.json(dbUsersData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // put update thought by ID
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      {
+        _id: req.params.thoughtId,
+      },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((updateThought) => {
+        if (!updateThought) {
+          return res.status(404).json({
+            message: "No thought found",
+          });
+        }
+        return res.json({
+          message: "Success",
+        });
+      })
+      .catch((err) => res.json(err));
+  },
+  // delete remove a thought by id
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({
+      _id: req.params.thoughtId,
+    })
+      .then((deleteThought) => {
+        if (!deleteThought) {
+          return res.status(404).json({
+            message: "No Thought Found",
+          });
+        }
+        return User.findOneAndUpdate(
+          {
+            thoughts: req.params.thoughtId,
+          },
+          {
+            $pull: {
+              thoughts: req.params.thoughtId,
+            },
+          },
+          {
+            new: true,
+          }
+        );
+      })
+      .then((dbUsersData) => {
+        if (!dbUserData) {
+          res.status(404).json({
+            message: "No thought found",
+          });
+          return;
+        }
+        res.json(dbUsersData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
---
+++ controllers/user-controller.js
@@ -0,0 +1,118 @@
+const { User } = require("../models");
+
+module.exports = {
+  //get all users
+  getUser(req, res) {
+    User.find({})
+      .select("-__v")
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  //get single user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // post a new user
+  createUser(req, res) {
+    Tags.create(req.body)
+      .then((tag) => {
+        return Post.findOneAndUpdate(
+          { _id: req.body.postId },
+          // Use $addToSet instead of $push to avoid duplicates
+          { $addToSet: { tags: tag._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "Tag created, but found no post with that ID" })
+          : res.json("Created the tag 🎉")
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      {
+        _id: params.id,
+      },
+      body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({
+            message: "No user found with this id.",
+          });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({
+      _id: params.id,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({
+            message: "No user found with this id.",
+          });
+          return;
+        }
+        return dbUserData;
+      })
+      .then((dbUserData) => {
+        User.updateMany(
+          {
+            _id: {
+              $in: dbUserData.friends,
+            },
+          },
+          {
+            $pull: {
+              friends: params.userId,
+            },
+          }
+        )
+          .then(() => {
+            //deletes user's thought associated with id
+            Thought.deleteMany({
+              username: dbUserData.username,
+            })
+              .then(() => {
+                res.json({
+                  message: "User deleted successfully",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
