1cbb73ad680dfffba1543b2b15f41e7d848e7717
Zeth <zethsatentes@yahoo.com>
2022-05-24 23:38:07 -0700

    tried fixing errors

--- models/Thought.js
+++ models/Thought.js
@@ -1,37 +1,4 @@
-const { User, Thought, Types, Schema, model } = require("mongoose");
-
-const ThoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      minlength: 1,
-      maxlength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) =>
-        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm:a"),
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    reactions: [reactionSchema],
-  },
-  {
-    toJSON: {
-      getters: true,
-      virtuals: true,
-    },
-    id: false,
-  }
-);
-
-ThoughtSchema.virtual("reactionCount").get(function () {
-  return this.reactions.length;
-});
+const { Types, Schema, model } = require("mongoose");
 
 //ReactionSchema
 const reactionSchema = new Schema({
@@ -60,6 +27,40 @@ const reactionSchema = new Schema({
     virtuals: true,
   },
   id: false,
+});
+
+// Thought Schema
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm:a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 const Thought = model("Thought", ThoughtSchema);
